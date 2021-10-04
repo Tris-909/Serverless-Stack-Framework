4a89d605100403d7b3d3f28cdb544b3f7d0e3986
Tri Tran <tranminhtri9090@gmail.com>
2021-10-04 20:53:21 +1000

    Construct get endpoint

---
+++ get.js
@@ -0,0 +1,19 @@
+import handler from './libs/handler-libs';
+import dynamoDB from './libs/dynamo-lib';
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: "123",
+            noteId: event.pathParameters.id,
+        }
+    };
+
+    const result = await dynamoDB.get(params);
+    if (!result.Item) {
+        throw new Error('Item not found')
+    }
+
+    return result.Item
+});
